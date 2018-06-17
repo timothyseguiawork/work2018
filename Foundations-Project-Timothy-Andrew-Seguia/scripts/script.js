f8906073090e8b1c65ad0fa8c2fa55919d73412f
timothyseguiawork <timothyseguiawork@gmail.com>
2018-06-17 09:30:53 -0400

    Add files via upload

---
+++ Foundations-Project-Timothy-Andrew-Seguia/scripts/script.js
@@ -0,0 +1,6 @@
+/*var myHeading = document.querySelector("h1");
+myHeading.textContent="Hello World!";
+*/
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
